c20b2357216f7018606f268aa9162326ffe0d299
lambdacdm <54066054+lambdacdm@users.noreply.github.com>
2020-11-01 13:24:01 +0800

    Add files via upload

--- computational.h
+++ computational.h
@@ -11,6 +11,7 @@
 #include<algorithm>
 #include<deque>
 #include<thread>
+#include<tuple>
 //#include<execution>
 #include<utility>
 using std::cerr;
@@ -24,6 +25,7 @@ using std::pair;
 using std::ref;
 using std::string;
 using std::swap;
+using std::tuple;
 //using std::thread;
 using std::vector;
 
@@ -208,6 +210,7 @@ public:
     Matrix<DM> &operator-=(const Matrix<DM> &);
 
     template <class DB> friend DB Get(const Matrix<DB> &, int, int);
+    template <class DB> friend void Make(Matrix<DB> &, int, int, DB);
     template<class DB> friend void Clear(Matrix<DB>&);
     template<class DB> friend void Disp(const Matrix<DB>&);
     template <class DB> friend Matrix<DB> Generate(int, int);
@@ -217,6 +220,10 @@ public:
     template<class DB> friend Matrix<DB> Transpose(const Matrix<DB>&); 
     template<class DB> friend vector<DB> Diagonal(const Matrix<DB>&);
     template <class DB> friend Matrix<DB> DiagonalMatrix(const vector<DB> &);
+    template <class DB> friend DB Min(const Matrix<DB>&);
+    template <class DB> friend DB Max(const Matrix<DB>&);
+    template <class DB> friend DB ColumnMin(const Matrix<DB> &,int );
+    template <class DB> friend DB ColumnMax(const Matrix<DB> &,int);
     template<class DB> friend int RowSize(const Matrix<DB>&);
     template<class DB> friend int ColumnSize(const Matrix<DB>&);
     template <class DB> friend void Resize(Matrix<DB> &,int,int);
@@ -293,6 +300,21 @@ private:
     int column_num;
 };
 template <class DB> const Matrix<DB> EmptyMatrix = Matrix<DB>(0, 0);
+
+//图像绘制
+template <class DG>
+class Graphics
+{
+    public:
+        Graphics();
+        Graphics(const Matrix<bool> &, const std::pair<DG, DG> &, const std::pair<DG, DG> &);
+        void show();
+
+    private:
+        Matrix<bool> pixel;
+        std::pair<DG,DG> xrange;
+        std::pair<DG,DG> yrange;
+};
 
 //积分
 template <class DI> DI Integrate(std::function<DI(DI)>,DI,DI,string);
@@ -1266,6 +1288,15 @@ template <class DB> DB Get(const Matrix<DB> &A, int r, int c)
 {
     return A.value[r][c];
 }
+template <class DB> void Make(Matrix<DB> &A, int r, int c, DB val)
+{
+    if(r<0 || r>=A.row_num || c<0 || c>=A.column_num)
+    {
+        cerr<<"错误：矩阵下标访问越界\n";
+        return;
+    }
+    A.value[r][c]=val;
+}
 template<class DB> void Clear(Matrix<DB> &a)
 {
     a.value.clear();
@@ -1338,6 +1369,62 @@ template<class DB> Matrix<DB> DiagonalMatrix(const vector<DB> &d)
     for(int i=0;i<n;++i)
         A.value[i][i]=d[i];
     return A;
+}
+template <class DB> DB Min(const Matrix<DB> &a)
+{
+    int rows=a.row_num;
+    int columns=a.column_num;
+    if(rows==0 || columns==0)
+        return DB(0);
+    DB minimum = a.value[0][0];
+    for(int i=0;i<rows;++i)
+        for(int j=0;j<columns;++j)
+            if(a.value[i][j]<minimum)
+                minimum=a.value[i][j];
+    return minimum;
+}
+template <class DB> DB Max(const Matrix<DB> &a)
+{
+    int rows=a.row_num;
+    int columns=a.column_num;
+    if(rows==0 || columns==0)
+        return DB(0);
+    DB maximum = a.value[0][0];
+    for(int i=0;i<rows;++i)
+        for(int j=0;j<columns;++j)
+            if(a.value[i][j]>maximum)
+                maximum=a.value[i][j];
+    return maximum;
+}
+template <class DB> DB ColumnMin(const Matrix<DB> &a,int j)
+{
+    int rows=a.row_num;
+    int columns=a.column_num;
+    if(j<0 || j>=columns)
+    {
+        std::cerr<<"错误：列数超出矩阵范围。\n";
+        return DB(0);
+    }
+    DB minimum=a.value[0][j];
+    for(int i=1;i<rows;++i)
+        if(a.value[i][j]<minimum)
+            minimum=a.value[i][j];
+    return minimum;
+}
+template <class DB> DB ColumnMax(const Matrix<DB> &a,int j)
+{
+    int rows=a.row_num;
+    int columns=a.column_num;
+    if(j<0 || j>=columns)
+    {
+        std::cerr<<"错误：列数超出矩阵范围。\n";
+        return DB(0);
+    }
+    DB maximum=a.value[0][j];
+    for(int i=1;i<rows;++i)
+        if(a.value[i][j]>maximum)
+            maximum=a.value[i][j];
+    return maximum;
 }
 template<class DB> Matrix<DB> Eye(int n)
 {
@@ -3797,6 +3884,140 @@ template<class DS,class DO> std::function<DO(DS)> DSolve(std::function<DO(DS,DO)
 template<class DS,class DO> std::function<DO(DS)> DSolve(std::function<DO(DS,DO)> f,const pair<DS,DO> &initial)
 {
     return [=](DS x) { return DSolve(f, initial, x); };
+}
+
+//图像绘制
+template<class DG> Graphics<DG>::Graphics()
+{
+    pixel = Matrix<bool>(0, 0);
+    xrange = std::pair<DG, DG>{0, 0};
+    yrange = std::pair<DG, DG>{0, 0};
+}
+template<class DG> Graphics<DG>::Graphics
+(const Matrix<bool> &d, const std::pair<DG, DG> &xr, const std::pair<DG, DG> &yr)
+{ 
+    pixel=d;
+    xrange=xr;
+    yrange = yr;
+}
+template<class DG> void Graphics<DG>::show()
+{
+    int n=RowSize(pixel);
+    int m=ColumnSize(pixel);
+    std::cout << '\n';
+    for (int i = 0;i<n;++i)
+    {
+        for(int j=0;j<m;++j)
+        {
+            if(Get(pixel,i,j))
+                std::cout<<'*';
+            else
+                std::cout << ' ';
+        }
+        std::cout << '\n';
+    }
+    std::cout << '\n';
+}
+template <class DG> Graphics<DG> Plot
+(const Matrix<DG> &data,std::pair<DG,DG> xr,std::pair<DG,DG> yr,unsigned xdots, unsigned ydots)
+{
+    DG xmin=std::get<0>(xr);
+    DG xmax=std::get<1>(xr);
+    DG ymin=std::get<0>(yr);
+    DG ymax=std::get<1>(yr);
+    Matrix<bool> pixelmatrix(ydots+1, xdots+1);
+    DG xh,yh;
+    if(xdots==0)
+        xh = 1;
+    else
+        xh = (xmax - xmin) / xdots;
+    if(ydots==0)
+        yh = 1;
+    else
+        yh = (ymax - ymin) / ydots;
+    const int datarows=RowSize(data);
+    for (int i = 0; i < datarows;++i)
+    {
+        int dotcolumn=int((Get(data,i,0)-xmin)/xh);
+        int dotrow = int((ymax-Get(data, i, 1)) / yh);
+        Make(pixelmatrix,dotrow,dotcolumn,true);
+    }
+    Graphics<DG> gr(pixelmatrix, xr, yr);
+    gr.show();
+    return gr;
+}
+template <class DG> Graphics<DG> Plot(const Matrix<DG> &data, unsigned xdots, unsigned ydots)
+{
+    DG xmin = ColumnMin(data, 0);
+    DG xmax = ColumnMax(data, 0);
+    DG ymin = ColumnMin(data, 1);
+    DG ymax = ColumnMax(data, 1);
+    std::pair<DG,DG> xr({xmin,xmax});
+    std::pair<DG,DG> yr({ymin, ymax});
+    return Plot(data, xr, yr, xdots, ydots);
+}
+template <class DG> Graphics<DG> Plot(const Matrix<DG> &data, unsigned xdots, string str)
+{
+    if(str=="unitscale")
+    {
+        DG xmin = ColumnMin(data, 0);
+        DG xmax = ColumnMax(data, 0);
+        DG ymin = ColumnMin(data, 1);
+        DG ymax = ColumnMax(data, 1);
+        std::pair<DG,DG> xr({xmin,xmax});
+        std::pair<DG,DG> yr({ymin, ymax});
+        unsigned ydots=1;
+        DG ratio=(ymax - ymin) / (xmax - xmin);
+        const DG maxratio = DG(3);
+        if(ratio>maxratio)
+        {
+            cerr<<"警告：y方向上函数值变动过大，已自动调整图像大小\n";
+            ratio=maxratio;
+        }
+        ydots=unsigned(xdots*ratio);
+        return Plot(data, xr, yr, xdots, ydots);
+    }
+    if(str=="square")
+    {
+        return Plot(data, xdots, xdots);
+    }
+    else
+    {
+        cerr<<"错误：未定义该方法\n";
+        return Plot(data,xdots,"unitscale");
+    }
+}
+template <class DG> Graphics<DG> Plot(const Matrix<DG> &data, unsigned xdots)
+{
+    return Plot(data, xdots, "unitscale");
+}
+template <class DG> Graphics<DG> Plot(const Matrix<DG> &data,string str)
+{
+    return Plot(data, RowSize(data), str);
+}
+template <class DG> Graphics<DG> Plot(const Matrix<DG> &data)
+{
+    return Plot(data, "unitscale");
+}
+template <class DG> Graphics<DG> Plot(const std::function<DG(DG)> &f,const vector<DG> &range)
+{
+    DG xmin=range[0];
+    DG xmax=range[1];
+    int xdots = 1;
+    if(range.size()>=3)
+        xdots=range[2];
+    else
+        xdots = 100;
+    Matrix<DG> data(xdots+1, 2);
+    DG xh = (xmax - xmin) / xdots;
+    DG pre = xmin;
+    for (int i = 0; i<=xdots;++i)
+    {
+        data(i, 0) = pre;
+        data(i, 1) = f(Get(data,i,0));
+        pre = pre + xh;
+    }
+    return Plot(data);
 }
 
 }
--- main.cpp
+++ main.cpp
@@ -1,21 +1,17 @@
 //#pragma GCC optimize(3,"inline","Ofast")
 //version 0.7.5
 #include <iostream>
-//#include "algebra.h"
 #include "computational.h"
-#include<ctime>
 #include <cmath>
 using namespace std;
 using namespace malg;
 int main()
 {
-    //auto f = [](double t, double y) { return 1-t+4*y; };
-    //cout << DSolve<double, double>(f, {0, 1}, 0.5,10,"backward Euler") << endl;
-    Matrix<double> A({{1,0},{0,1e-10}},"");
-    cout << Cond(A, 2) << endl;
+    Plot<double>([](double x) { return sin(x); },{0,2*Pi<double>});
+    //绘制sinx在x属于[0,2pi]的图像
     system("pause");
     return 0;
 }
                                     
                     
-                               
+                                  
